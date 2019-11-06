9c58fdc2c05d9c9472bccaa027dafaa18df15588
“Genpeng” <xgp1227@gmai.com>
2019-11-06 08:59:57 +0800

    Modify the solution of no. 230 problem in the LeetCode

--- PyLeetCode/tree/lc0230_kth_smallest_element_in_a_bst/solution.py
+++ PyLeetCode/tree/lc0230_kth_smallest_element_in_a_bst/solution.py
@@ -41,13 +41,15 @@ Author: StrongXGP (xgp1227@gmail.com)
 Date:   2019/06/21
 """
 
+from typing import Optional
 from PyLeetCode.entity.tree import *
 
 
 class Solution1:
-    def kth_smallest(self, root: TreeNode, k: int) -> int:
+    def kth_smallest(self, root: Optional[TreeNode], k: int) -> int:
         """
-        解法一：中序遍历二叉搜索树，并将遍历的结果存储起来，返回结果中索引为k-1的元素即为第k小的元素
+        解法一：利用二叉搜索树中序遍历的性质
+        中序遍历二叉搜索树，并将遍历的结果存储起来，返回结果中索引为k-1的元素即为第k小的元素
         时间复杂度：O(n)
         空间复杂度：O(n)
 
@@ -55,23 +57,24 @@ class Solution1:
         :param k: int, an integer used to specify the element we want to fetch
         :return: int, the kth smallest element
         """
-        elements, stack = [], []
-        curr = root
+        elems = []
+        stack, curr = [], root
         while curr or stack:
             if curr:
                 stack.append(curr)
                 curr = curr.left
             else:
                 node = stack.pop()
-                elements.append(node.val)
+                elems.append(node.val)
                 curr = node.right
-        return elements[k-1]
+        return elems[k-1]
 
 
 class Solution2:
-    def kth_smallest(self, root: TreeNode, k: int) -> int:
+    def kth_smallest(self, root: Optional[TreeNode], k: int) -> int:
         """
-        解法二：解法二在解法一的基础上进行改进，同样地，中序遍历二叉搜索树，但是当遍历到第k个元素时就停止遍历
+        解法二：利用二叉搜索树中序遍历的性质
+        解法二在解法一的基础上进行改进，同样地，中序遍历二叉搜索树，但是当遍历到第k个元素时就停止遍历
         时间复杂度：O(n)
         空间复杂度：O(n)
 
@@ -91,4 +94,30 @@ class Solution2:
                 if k == 0:
                     return node.val
                 curr = node.right
-        raise Exception("[ERROR] The value of k is illegal!!!")
+        raise Exception("[ERROR] The BST is null or the value of k is illegal!!!")
+
+    def kth_smallest_v2(self, root: TreeNode, k: int) -> int:
+        """
+        解法二：利用二叉搜索树中序遍历的性质
+        解法二在解法一的基础上进行改进，同样地，中序遍历二叉搜索树，但是当遍历到第k个元素时就停止遍历
+        时间复杂度：O(n)
+        空间复杂度：O(n)
+
+        :param root: TreeNode, the root of BST
+        :param k: int, an integer used to specify the element we want to fetch
+        :return: int, the kth smallest element
+        """
+        stack, curr = [], root
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            node = stack.pop()
+            k -= 1
+            if k == 0:
+                return node.val
+            curr = node.right
+        # 抛出异常的情况有：
+        # 1. BST is null
+        # 2. k < 1 or k > n, where n is the number of nodes in the bst
+        raise Exception("[ERROR] The BST is null or the value of k is illegal!!!")
