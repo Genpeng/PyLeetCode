83692471664b61ae79a19ca60415fbc139c0ff0a
Genpeng Xu <xgp1227@gmai.com>
2019-12-31 08:58:46 +0800

    Modify the solution of no. 144 problem in the LeetCode

--- PyLeetCode/tree/lc0144_binary_tree_preorder_traversal/solution.py
+++ PyLeetCode/tree/lc0144_binary_tree_preorder_traversal/solution.py
@@ -41,16 +41,16 @@ class Solution1:
         :param root: TreeNode, the root of BST
         :return: list, the preorder traversal of BST
         """
-        res = []
-        self._preorder_traversal(root, res)
-        return res
+        ans = []
+        self._preorder_traversal(root, ans)
+        return ans
 
-    def _preorder_traversal(self, root: Optional[TreeNode], res: List[int]) -> None:
+    def _preorder_traversal(self, root: Optional[TreeNode], ans: List[int]) -> None:
         if root is None:
             return
-        res.append(root.val)
-        self._preorder_traversal(root.left, res)
-        self._preorder_traversal(root.right, res)
+        ans.append(root.val)
+        self._preorder_traversal(root.left, ans)
+        self._preorder_traversal(root.right, ans)
 
 
 class Solution2:
@@ -63,18 +63,18 @@ class Solution2:
         :param root: TreeNode, the root of BST
         :return: list, the preorder traversal of BST
         """
-        res = []
-        if root is None:
-            return res
+        ans = []
+        if not root:
+            return ans
         stack = [root]
         while stack:
-            node = stack.pop()
-            res.append(node.val)
-            if node.right:
-                stack.append(node.right)
-            if node.left:
-                stack.append(node.left)
-        return res
+            curr = stack.pop()
+            ans.append(curr.val)
+            if curr.right:
+                stack.append(curr.right)
+            if curr.left:
+                stack.append(curr.left)
+        return ans
 
     def preorder_traversal_v2(self, root: Optional[TreeNode]) -> List[int]:
         """
@@ -85,16 +85,36 @@ class Solution2:
         :param root: TreeNode, the root of BST
         :return: list, the preorder traversal of BST
         """
-        res, stack = [], [root]
+        ans, stack = [], [root]
         while stack:
             node = stack.pop()
             if node:
-                res.append(node.val)
+                ans.append(node.val)
                 stack.append(node.right)
                 stack.append(node.left)
-        return res
+        return ans
 
     def preorder_traversal_v3(self, root: Optional[TreeNode]) -> List[int]:
+        """
+        解法二：迭代
+        时间复杂度：O(n)
+        空间复杂度：O(n)
+
+        :param root: TreeNode, the root of BST
+        :return: list, the preorder traversal of BST
+        """
+        ans = []
+        curr, stack = root, []
+        while curr or stack:
+            if curr:
+                ans.append(curr.val)
+                stack.append(curr)
+                curr = curr.left
+            else:
+                curr = stack.pop().right
+        return ans
+
+    def preorder_traversal_v4(self, root: Optional[TreeNode]) -> List[int]:
         """
         解法二：迭代（推荐）
         时间复杂度：O(n)
@@ -103,12 +123,12 @@ class Solution2:
         :param root: TreeNode, the root of BST
         :return: list, the preorder traversal of BST
         """
-        res, stack, curr = [], [], root
+        ans = []
+        curr, stack = root, []
         while curr or stack:
-            if curr:
-                res.append(curr.val)
+            while curr:
+                ans.append(curr.val)
                 stack.append(curr)
                 curr = curr.left
-            else:
-                curr = stack.pop().right
-        return res
+            curr = stack.pop().right
+        return ans
